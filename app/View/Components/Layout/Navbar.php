b541ed9059016373af923cae75b63d73c39bbb7e
a.cherashev <a.cherashev@cetera.ru>
2022-05-05 23:12:01 +0500

    logo

--- app/View/Components/Layout/Navbar.php
+++ app/View/Components/Layout/Navbar.php
@@ -2,10 +2,15 @@
 
 namespace App\View\Components\Layout;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class Navbar extends Component
 {
+    public array $navigationItems = [];
+
     /**
      * Create a new component instance.
      *
@@ -13,13 +18,18 @@ class Navbar extends Component
      */
     public function __construct()
     {
-        //
+        $this->navigationItems = [
+            ['label' => 'About', 'href' => '#about'],
+            ['label' => 'Projects', 'href' => '#portfolio'],
+            ['label' => 'Tutorials', 'href' => '#tutorials'],
+            ['label' => 'Contact', 'href' => '#contact'],
+        ];
     }
 
     /**
      * Get the view / contents that represent the component.
      *
-     * @return \Illuminate\Contracts\View\View|\Closure|string
+     * @return Application|Factory|View
      */
     public function render()
     {
