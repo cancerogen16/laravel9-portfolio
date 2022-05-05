60c555ae4a0967fba3f3f12abb61f60bda9a6ebf
a.cherashev <a.cherashev@cetera.ru>
2022-05-05 23:53:34 +0500

    footer

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -8,10 +8,12 @@
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-<body class="antialiased">
-<x-layout.navbar></x-layout.navbar>
-{{ $slot }}
-<footer>Footer</footer>
+<body class="antialiased text-gray-800 dark:text-gray-200">
+<div class="min-h-screen bg-gray-100 dark:text-gray-900 pt-24">
+    <x-layout.navbar></x-layout.navbar>
+    {{ $slot }}
+    <x-layout.footer></x-layout.footer>
+</div>
 <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
